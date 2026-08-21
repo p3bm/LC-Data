2beb490c1108ee24584dbee132ad5a96e5b8dbc7
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2026-08-21 09:33:23 +0100

    Refactor app.py to improve code structure and clarity

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ if uploaded_file is not None:
     calculate_lcap = st.toggle("Calculate LCAP (Relative Peak Area) from the merged data")
 
     if not calculate_lcap:
-        st.dataframe(merged_df_display)
+        st.dataframe(merged_df_display.sort_values(by='Sample Name', ascending=True))
         final_df = merged_df_display
     else:
         # Identify retention time columns
@@ -175,9 +175,8 @@ if uploaded_file is not None:
         lcap_data = selected_data.div(total_areas, axis=0) * 100
 
         lcap_results = lcap_data.round(1)
-        st.dataframe(lcap_results)
-
-        final_df = lcap_results
+        final_df = lcap_results.sort_values(by='Sample Name', ascending=True)
+        st.dataframe(final_df)
 
     # Download intermediate table
     st.download_button(
